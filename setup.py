636e90043c81ddfb12d33df95c43ac67e65104b5
David Karlsson <dkarlss@gmail.com>
2019-10-28 22:00:58 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='flowlog_pprint',
-    version='0.0.1',
+    version='0.0.2',
     packages=setuptools.find_packages(),
     install_requires=['colorama'],
     python_requires='>=3.5.0',

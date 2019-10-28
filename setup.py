d1227bafd3abe28be059806177729bc4d824de0e
David Karlsson <dkarlss@gmail.com>
2019-10-28 21:50:55 +0100

    fix name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name='flowlog-pprint',
+    name='flowlog_pprint',
     version='0.0.1',
     packages=setuptools.find_packages(),
     install_requires=['colorama'],
     python_requires='>=3.5.0',
     entry_points={
         'console_scripts': [
-            'flowlog_pprint = flowlog_prettyprint.__main__:main'
+            'flowlog_pprint = flowlog_pprint.__main__:main'
         ]
     },
     url="https://github.com/d-karlss/flowlog-pprint",
